6119eb18bb9195ae65794e9f6a1a4cbc89382572
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2025-01-02 20:37:10 +0100

    Update LSS Leitstellenansicht Edit+Zuweisungsbuttons.user.js
    // fixed Button-Icon Problem by changing to glyphicons

--- LSS Leitstellenansicht Edit+Zuweisungsbuttons.user.js
+++ LSS Leitstellenansicht Edit+Zuweisungsbuttons.user.js
@@ -40,7 +40,7 @@
             const url = `https://www.leitstellenspiel.de/vehicles/${vehicleId}/set_fms/${targetStatus}`;
             try {
                 await fetch(url);
-                //console.log(`Status für Fahrzeug ${vehicleId} auf ${targetStatus} gesetzt.`);
+                console.log(`Status für Fahrzeug ${vehicleId} auf ${targetStatus} gesetzt.`);
             } catch (error) {
                 console.error(`Fehler beim Setzen des Status für Fahrzeug ${vehicleId}:`, error);
             }
@@ -76,7 +76,7 @@
         // Edit-Button erstellen
         const editButton = document.createElement('a');
         editButton.className = 'btn btn-xs btn-default';
-        editButton.innerHTML = '<svg class="svg-inline--fa fa-users" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="users" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="currentColor" d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"></path></svg>';
+        editButton.innerHTML = '<span class="glyphicon glyphicon-user"></span>';
         editButton.setAttribute('href', `${href}/zuweisung`);
         editButton.setAttribute('target', '_blank');
         editButton.addEventListener('click', stopParentClick);
@@ -157,8 +157,7 @@
 
         const noneLeitstelleButton = document.createElement('button');
         noneLeitstelleButton.className = 'btn btn-xs btn-danger pull-right';
-        noneLeitstelleButton.innerHTML = '<svg class="svg-inline--fa fa-times" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M137.4 137.4c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3l-128 128c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0z"></path></svg>';
-
+        noneLeitstelleButton.innerHTML = '<span class="glyphicon glyphicon-remove"></span>';
         noneLeitstelleButton.addEventListener('click', (event) => {
             event.preventDefault();
             event.stopPropagation();
@@ -183,8 +182,7 @@
             const assignButton = document.createElement('button');
             assignButton.className = 'btn btn-xs btn-success pull-right';
             assignButton.style.marginLeft = '1ch';
-            assignButton.innerHTML = '<svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"></path></svg>';
-
+            assignButton.innerHTML = '<span class="glyphicon glyphicon-ok"></span>';
             assignButton.addEventListener('click', (event) => {
                 event.preventDefault();
                 event.stopPropagation();
