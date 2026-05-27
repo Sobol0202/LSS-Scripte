c290b64ee80af95dd3fb6be13f95dad08285d384
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2026-05-27 18:19:00 +0200

    Update user script version and improve button handling

--- LSS Fahrzeugnavigation vor Bearbeiten.user.js
+++ LSS Fahrzeugnavigation vor Bearbeiten.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Fahrzeugnavigation vor Bearbeiten
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Verschiebt die Fahrzeugnavigationspfeile vor den Bearbeiten-Button
 // @author       Sobol
 // @match        https://www.leitstellenspiel.de/vehicles/*
@@ -11,36 +11,61 @@
 (function () {
     'use strict';
 
-    const groups = document.querySelectorAll('.btn-group.pull-right');
+    let running = false;
 
-    let navGroup;
-    let actionGroup;
-    let editButton;
+    function fixVehicleButtons() {
+        if (running) return;
+        running = true;
 
-    for (const group of groups) {
-        if (group.querySelector('.glyphicon-arrow-left, .glyphicon-arrow-right')) {
-            navGroup = group;
-        }
+        try {
+            const editButton = document.querySelector('a[href^="/vehicles/"][href$="/edit"]');
+            if (!editButton) return;
 
-        const edit = group.querySelector('a[href$="/edit"] .glyphicon-pencil');
-        if (edit) {
-            actionGroup = group;
-            editButton = edit.closest('a');
+            const actionGroup = editButton.closest('.btn-group');
+            if (!actionGroup) return;
+
+            const leftButton = document
+                .querySelector('.glyphicon-arrow-left')
+                ?.closest('a');
+
+            const rightButton = document
+                .querySelector('.glyphicon-arrow-right')
+                ?.closest('a');
+
+            const buttons = [leftButton, rightButton].filter(Boolean);
+
+            if (!buttons.length) return;
+
+            buttons.forEach(button => {
+                button.classList.remove('btn-xs');
+                button.classList.add('btn-default');
+                button.removeAttribute('style');
+
+                actionGroup.insertBefore(button, editButton);
+            });
+
+            document.querySelectorAll('.btn-group.pull-right, .btn-group').forEach(group => {
+                if (!group.children.length) group.remove();
+            });
+
+        } finally {
+            running = false;
         }
     }
 
-    if (!navGroup || !actionGroup || !editButton) return;
+    function scheduleFix() {
+        requestAnimationFrame(fixVehicleButtons);
+        setTimeout(fixVehicleButtons, 100);
+        setTimeout(fixVehicleButtons, 300);
+        setTimeout(fixVehicleButtons, 700);
+        setTimeout(fixVehicleButtons, 1200);
+        setTimeout(fixVehicleButtons, 2500);
+    }
 
-    const navButtons = Array.from(
-        navGroup.querySelectorAll('a.btn')
-    );
+    scheduleFix();
 
-    navButtons.forEach(button => {
-        button.classList.remove('btn-xs');
-        button.classList.add('btn-default');
-
-        actionGroup.insertBefore(button, editButton);
+    new MutationObserver(scheduleFix).observe(document.documentElement, {
+        childList: true,
+        subtree: true
     });
-
-    navGroup.remove();
 })();
